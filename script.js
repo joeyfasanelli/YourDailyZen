7c5b6d70ad2ee6cc4e80c3d341d54b981eff3cbf
Your Name <you@example.com>
2022-08-22 16:20:25 -0400

    Added HTML and CSS styling, scrapped API

---
+++ script.js
@@ -0,0 +1,12 @@
+const api_url ="https://favqs.com/api/qotd";
+const $button = $('#search');
+const $author = $('#author')
+const $quote = $('#quote')
+
+
+
+function generateQuote() {
+
+}
+
+$button.on('click', generateQuote) 
