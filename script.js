dd395f20e5a04002bd871ed031cb316e8bc66f2d
Your Name <you@example.com>
2022-08-22 17:52:33 -0400

    added new API

--- script.js
+++ script.js
@@ -5,8 +5,25 @@ const $quote = $('#quote')
 
 
 
-function generateQuote() {
-
+async function getapi(url)
+{
+  const response = await fetch(url);
+  var data = await response.json();
+  console.log(data);
 }
 
-$button.on('click', generateQuote) 
+getapi(api_url);
+
+
+function generateQuote() {
+    $.ajax(api_url).then(
+        function(data) {
+            $quote.text(data.body)
+            $author.text(data.author)
+        }
+    )
+}
+
+generateQuote();
+
+$button.on('click', generateQuote);
