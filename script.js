2a4a7609f002b1e7adca68521cd2c97876f02d86
Your Name <you@example.com>
2022-08-23 11:36:23 -0400

    added clickable button

--- script.js
+++ script.js
@@ -1,29 +1,65 @@
 const api_url ="https://favqs.com/api/qotd";
-const $button = $('#search');
+const $button = $('button');
 const $author = $('#author')
 const $quote = $('#quote')
 
-
-
 async function getapi(url)
-{
-  const response = await fetch(url);
-  var data = await response.json();
-  console.log(data);
-}
-
-getapi(api_url);
+    {
+      const response = await fetch(url);
+      var data = await response.json();
+      console.log(data);
+    }
 
 
-function generateQuote() {
-    $.ajax(api_url).then(
-        function(data) {
-            $quote.text(data.body)
-            $author.text(data.author)
-        }
-    )
-}
 
-generateQuote();
 
-$button.on('click', generateQuote);
+
+// $('button').on('click', handleGetData);
+
+// function handleGetData(event) {
+//     event.preventDefault();
+
+// $.ajax({
+//     url:"https://favqs.com/api/qotd"
+//     })
+//     .then(
+//       (data) => {
+//         $quote.text(data.body);
+//         $author.text(data.author);
+        
+//       });
+//     }
+
+
+
+
+
+// function generateQuote() {
+//     $.ajax(api_url).then(
+//         function(data) {
+//             $quote.text(data.body)
+//             $author.text(data.author)
+//         }
+//     )
+// }
+
+// generateQuote();
+
+
+
+
+
+
+
+// $button.on('click', generateQuote);
+
+// function generateQuote() {
+
+// $.ajax(api_url).then(
+//     function(data){
+        
+//         console.log('quote is ready!');
+//             console.log(data);
+//             $quote.text(data.body)
+//             $author.text(data.$author)
+// })};
