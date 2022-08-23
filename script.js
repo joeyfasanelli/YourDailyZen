0375aa78b9c335323324c7bd4b7ca9a13cac6f03
Your Name <you@example.com>
2022-08-23 15:21:09 -0400

    Added final functionality for the site to work properly

--- script.js
+++ script.js
@@ -1,65 +1,14 @@
 const api_url ="https://favqs.com/api/qotd";
-const $button = $('button');
+const quoteButton = document.getElementById("search")
 const $author = $('#author')
 const $quote = $('#quote')
 
-async function getapi(url)
-    {
-      const response = await fetch(url);
-      var data = await response.json();
-      console.log(data);
-    }
+function handleGetData(event) {
+    event.preventDefault()
+    $.ajax(api_url).then(
+        function(data){
+        console.log(data);
+        $quote.text(data.quote.body)
+        $author.text("--" + data.quote.author)
+})};
 
-
-
-
-
-// $('button').on('click', handleGetData);
-
-// function handleGetData(event) {
-//     event.preventDefault();
-
-// $.ajax({
-//     url:"https://favqs.com/api/qotd"
-//     })
-//     .then(
-//       (data) => {
-//         $quote.text(data.body);
-//         $author.text(data.author);
-        
-//       });
-//     }
-
-
-
-
-
-// function generateQuote() {
-//     $.ajax(api_url).then(
-//         function(data) {
-//             $quote.text(data.body)
-//             $author.text(data.author)
-//         }
-//     )
-// }
-
-// generateQuote();
-
-
-
-
-
-
-
-// $button.on('click', generateQuote);
-
-// function generateQuote() {
-
-// $.ajax(api_url).then(
-//     function(data){
-        
-//         console.log('quote is ready!');
-//             console.log(data);
-//             $quote.text(data.body)
-//             $author.text(data.$author)
-// })};
