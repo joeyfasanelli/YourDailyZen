098b9e2934f312ac996f38c43de83b703f71bd41
Joey Fasanelli <joeyfas1704@gmail.com>
2022-08-24 13:01:16 -0400

    issue with deployed site not including background image

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const api_url ="https://favqs.com/api/qotd";
-const quoteButton = document.getElementById("search")
+const $button = $("#search")
 const $author = $('#author')
 const $quote = $('#quote')
+
 
 function handleGetData(event) {
     event.preventDefault()
     $.ajax(api_url).then(
         function(data){
         console.log(data);
-        $quote.text(data.quote.body)
+        $quote.text(data.quote.body);
         $author.text("--" + data.quote.author)
+        
 })};
+
 
